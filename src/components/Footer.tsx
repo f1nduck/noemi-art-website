92d1473087ad38583683200e925f374f048c0254
f1nduck <finlaymair@gmail.com>
2024-02-18 13:55:30 +0000

    Copyright text added to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,16 +9,22 @@ const Footer = ({ artistEmail, instagramLink }) => {
 
   return (
     <footer>
-      <div className="brand-icon">
-        {/* Insert your artist's brand icon image here */}
-        <img src="src\assets\images\logo1.png" alt="Artist Brand Icon" />
-      </div>
+      <div className="brand-container">
+        <div className="brand-icon">
+          {/* Insert your artist's brand icon image here */}
+          <img src="src\assets\images\logo1.png" alt="Artist Brand Icon" />
+        </div>  
+        <div className="copyright">
+          {/* Add the copyright text */}
+          © 2024 Noemi Naszarkowski
+        </div>
+      </div>  
       <div className="contact">
         <button onClick={handleEmailClick}>
-          <FaEnvelope /> {/*text for button can go here */}
+          <FaEnvelope /> {/* text for button can go here */}
         </button>
         <a href={instagramLink} target="_blank" rel="noopener noreferrer">
-          <FaInstagram /> {/*text for button can go here */}
+          <FaInstagram /> {/* text for button can go here */}
         </a>
       </div>
     </footer>
