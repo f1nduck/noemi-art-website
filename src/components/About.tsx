90f1a921bb80004a1b33770be0ef03ccad2f02a6
f1nduck <finlaymair@gmail.com>
2024-04-10 00:54:32 +0100

    mobile friendly about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,7 +8,7 @@ const About: React.FC = () => {
       <div className="content">
         <h1 className="heading">About</h1>
         <p className="paragraph">
-          Hello! My name is Noemi Naszarkowski, I am an artist by heart and a biologist by mind. Depicting and experiencing the world around me, especially nature, have been integral parts of me since I was a small child. I could spend hours exploring forests and meadows, identifying and drawing animal species. In my teens, I pursued portrait drawing, refining my skills with guidance from my father, an artist himself. Photography became another avenue for my creative expression, particularly in capturing portraits, and I’ve been hooked on film photography since I inherited my grandmother’s 1940s camera.
+          Hello! My name is Noemi Naszarkowski, I am an artist by heart and a biologist by mind. Depicting and experiencing the world around me, especially nature, have been integral parts of me since I was a small child. I could spend hours exploring forests and meadows, identifying and drawing animal species. In my teens, I pursued portrait drawing, refining my skills with guidance from my father, an artist himself.
 
           I've had the privilege of creating commissioned artwork and photography, including an album cover and magazine illustrations. I'm always eager to embark on new artistic endeavors and collaborative projects, so feel free to reach out to discuss potential collaborations or commissions.
         </p>
